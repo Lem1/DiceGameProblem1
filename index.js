0a60a55c6415f61a01a4f802bf301c6751be02c5
Craig Alford <crglfrd@gmail.com>
2019-04-12 14:04:41 -0700

    modified to object literal

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 ﻿/**
  * Created by bcuser on 4/4/19.
  */
-let image1 = document.getElementById("image1");
-let image2= document.getElementById("image2");
-let balance =document.getElementById("balance");
-let turnCount =document.getElementById("turnCount");
-let status =document.getElementById("status");
-let ButtonBet =document.getElementById("ButtonBet");
 
-let count =0;
-let bal =5; 
-function buttonClicked()
-{
-
-let ranNum1 = Math.floor((Math.random() * 6) + 1);
-let ranNum2 = Math.floor((Math.random() * 6) + 1);
-image1.src="./images/dice-"+ ranNum1 +".jpg";
-image2.src ="./images/dice-"+ ranNum2 + ".jpg";
-let sumnum= ranNum1 + ranNum2;
-
-if(ranNum1===ranNum2 || sumnum===7 || sumnum===11)
-{
-bal+=1;
-balance.innerHTML=bal;
-status.innerHTML="Win";
+// set jQuery-like variable
+var $ = function(id) {
+    return document.getElementByID(id);
 }
 
-else
-{
-bal -=1;
-balance.innerHTML=bal;
-status.innerHTML=" You Lost";
+const DiceGame = {
+    image1: $("image1"),
+    image2: $("image2"),
+    balance: $("balance"),
+    turnCount: $("turnCount"),
+    status: $("status"),
+    ButtonBet: $("ButtonBet"),
+    count: 0,
+    bal: 5, 
+    buttonClicked: function() {
+        // get random numbers between 1 and 6
+        let ranNum1 = Math.ceil((Math.random() * 6));
+        let ranNum2 = Math.ceil((Math.random() * 6));
+        // update dice images
+        this.image1.src="./images/dice-"+ ranNum1 +".jpg";
+        this.image2.src ="./images/dice-"+ ranNum2 + ".jpg";
+        
+        // add dice count together
+        let sumnum= ranNum1 + ranNum2;
+        if (ranNum1===ranNum2 || sumnum===7 || sumnum===11) {
+            // player won
+            bal+=1;
+            this.balance.innerHTML=bal;
+            this.status.innerHTML="Win";
+        } else {
+            // player lost
+            bal -=1;
+            this.balance.innerHTML=bal;
+            this.status.innerHTML=" You Lost";
+        }
 
-}
-
-count+=1;
-turnCount.innerHTML=count;
-if(count==45)
-{
-status.innerHTML="Game Over";
-ButtonBet.style.display==="none";
-}
-else if(bal===0)
-{
-status.innerHTML="Game Over!";
-ButtonBet.style.display="none";
-}
-
+        // update turn
+        count+=1;
+        
+        // check win or lose
+        this.turnCount.innerHTML=count;
+        if (count==45) {
+            this.status.innerHTML="Game Over - you take home $" + bal);
+            this.ButtonBet.addAttribute("disabled", "true");
+        } else if (bal===0) {  
+            this.status.innerHTML="Game Over - you lost...";
+            this.ButtonBet.addAttribute("disabled", "true");
+        }
+    }
 }
