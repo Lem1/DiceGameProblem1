531751ad96fd8a813ab55eba1281447bd2925862
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-04-12 16:03:37 -0700

    debugged event running on open

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 // set jQuery-like variable
 var $ = function(id) {
-    return document.getElementByID(id);
-}
+    return document.getElementById(id);
+};
 
 const DiceGame = {
     image1: $("image1"),
@@ -16,6 +16,7 @@ const DiceGame = {
     ButtonBet: $("ButtonBet"),
     count: 0,
     bal: 5, 
+
     buttonClicked: function() {
         // get random numbers between 1 and 6
         let ranNum1 = Math.ceil((Math.random() * 6));
@@ -28,33 +29,30 @@ const DiceGame = {
         let sumnum= ranNum1 + ranNum2;
         if (ranNum1===ranNum2 || sumnum===7 || sumnum===11) {
             // player won
-            bal+=1;
-            this.balance.innerHTML=bal;
+            this.bal+=1;
+            this.balance.innerHTML=this.bal;
             this.status.innerHTML="Win";
         } else {
             // player lost
-            bal -=1;
-            this.balance.innerHTML=bal;
+            this.bal -=1;
+            this.balance.innerHTML=this.bal;
             this.status.innerHTML=" You Lost";
         }
 
         // update turn
-        count+=1;
+        this.count+=1;
         
         // check win or lose
-        this.turnCount.innerHTML=count;
-        if (count==45) {
-            this.status.innerHTML="Game Over - you take home $" + bal);
-            this.ButtonBet.addAttribute("disabled", "true");
-        } else if (bal===0) {  
+        this.turnCount.innerHTML=this.count;
+        if (this.count==45) {
+            this.status.innerHTML="Game Over - you take home $" + this.bal;
+            this.ButtonBet.disabled = true;
+        } else if (this.bal===0) {  
             this.status.innerHTML="Game Over - you lost...";
-            this.ButtonBet.addAttribute("disabled", "true");
+            this.ButtonBet.disabled = true;;
         }
     }
 }
 
-$(document).ready(function() {
-    $("ButtonBet").click(function() {
-        DiceGame.buttonClicked();
-    })
-});
+$("ButtonBet").addEventListener("click", function() { DiceGame.buttonClicked(); } );
+
